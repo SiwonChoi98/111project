afb9eec998a8dd790d4fc6f8f0d661b9c862b2be
SiwonChoi98 <118884862+SiwonChoi98@users.noreply.github.com>
2023-06-13 10:52:02 +0900

    플레이어
    애니메이션 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,14 +14,13 @@ public class GameManager : MonoBehaviour
     }
     private void LateUpdate()
     {
+        Gui();
+    }
+    private void Gui()
+    {
         if (!player.isShield)
             shieldTimeImage.fillAmount = player.curShieldCoolTime / player.maxShieldCoolTime;
         else
             shieldTimeImage.fillAmount = 0;
-
-        //if (player.isDodgeReady)
-        //    playerDodgeCoolImage.fillAmount = 0;
-        //else
-        //    playerDodgeCoolImage.fillAmount = player.dodgeCoolTime / player.dodgeCoolTimeMax;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,12 @@ public class Player : MonoBehaviour
 {
     
     private Rigidbody2D rigid; //물리
-
+    private Animator anim; //애니메이션
     //점프
-    private const float jumpPower = 45; //점프 힘
+    private const float jumpPower = 60; //점프 힘
     private bool isJump = false; //점프 가능한지 체크
     //쉴드
-    public float maxShieldCoolTime = 5;
+    public float maxShieldCoolTime = 3;
     public float curShieldCoolTime = 0;
     public bool isShield = false; //쉴드 상태로 갈 수 있는지
     public bool isShieldState = false; //현재 쉴드 상태인지
@@ -20,6 +20,7 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     public void Initialized()
     {
@@ -30,12 +31,28 @@ public class Player : MonoBehaviour
         if (!isJump) //점프 가능한 상태일 때 점프 기능
         {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetTrigger("DoJump");
             isJump = true;
         }
     }
     public void Attack()
     {
         Debug.Log("Attack");
+        int ran = Random.Range(0,3);
+        switch (ran)
+        {
+            case 0:
+                anim.SetTrigger("DoAttack1");
+                break;
+            case 1:
+                anim.SetTrigger("DoAttack2");
+                break;
+            case 2:
+                anim.SetTrigger("DoAttack3");
+                break;
+            default:
+                return;
+        }
     }
     public void Shield()
     {
@@ -43,6 +60,7 @@ public class Player : MonoBehaviour
         {
             isShield = false;
             isShieldState = true;
+            anim.SetTrigger("DoShield");
             Debug.Log("Shield");
         }
         
@@ -56,6 +74,7 @@ public class Player : MonoBehaviour
             if (curShieldCoolTime >= maxShieldCoolTime)
             {
                 isShield = true;
+                isShieldState = false;
                 curShieldCoolTime = 0;
             }
         }
@@ -69,6 +88,7 @@ public class Player : MonoBehaviour
         //바닥과 닿았는지 체크 후 점프 가능한 상태로 만들어줌
         if (collision.gameObject.CompareTag("Floor"))
         {
+            anim.SetTrigger("DoFall");
             isJump = false;
         }
     }
